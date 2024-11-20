0fe8de7153c2ab884648f602f01048a54e7d4e7a
stevmasterxd <stevenmanuel1234@gmail.com>
2024-11-19 21:26:54 -0400

    segundo commit

---
+++ src/components/Contador.vue
@@ -0,0 +1,23 @@
+<template>
+  <h1>Hola mundo</h1>
+  <p>{{ contador }}</p>
+  <button @click="incrementar">incrementar</button>
+  <button @click="decrementar">decrementar</button>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+const contador = ref(0);
+const incrementar = () => {
+  contador.value++;
+}
+const decrementar = () => {
+  contador.value--;
+}
+</script>
+
+<style scoped>
+p {
+  color: red;
+}
+</style>
